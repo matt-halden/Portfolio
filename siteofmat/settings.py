0f7d648b6c54438ee4683257de8e676316a2b5cf
Matt Halden <just.m.halden@gmail.com>
2024-09-25 15:40:22 -0700

    Remove Debug True

--- siteofmat/settings.py
+++ siteofmat/settings.py
@@ -30,7 +30,7 @@ if ENV_FILE:
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["mhalden-252c260ccb4e.herokuapp.com", "127.0.0.1", "localhost"]
 
