0c848b0af294d984d3745780221641f694aa7134
Matt Halden <just.m.halden@gmail.com>
2024-09-25 15:38:58 -0700

    updating allowed-hosts

--- siteofmat/settings.py
+++ siteofmat/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mhalden-252c260ccb4e.herokuapp.com/", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["mhalden-252c260ccb4e.herokuapp.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
